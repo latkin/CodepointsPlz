8850e00f795fed7f8515863bd398b94fac6249d6
Lincoln Atkinson <latkin@users.noreply.github.com>
2018-05-22 11:16:56 -0700

    Proper build/deploy - ProcessMentions

--- ProcessMentions/run.fs
+++ ProcessMentions/run.fs
@@ -1,27 +1,16 @@
-#if VS
-module run
-#else
-#r "System.Net.Http"
-#r "Newtonsoft.Json"
-#r "Microsoft.WindowsAzure.Storage"
-#r "System.Web"
-#r "System.Linq.Expressions"
-#r "System.Collections"
-#endif
+module ProcessMentions
 
 open System
 open System.Collections.Generic
-open System.Net
-open System.Net.Http
-open Microsoft.Azure.WebJobs.Host
-open Microsoft.Azure.WebJobs
-open Newtonsoft.Json
-open System.Text.RegularExpressions
-open LinqToTwitter
 open System.IO
+open System.Text.RegularExpressions
+open System.Web
+open Microsoft.Azure.WebJobs
+open Microsoft.Azure.WebJobs.Host
 open Microsoft.WindowsAzure.Storage
 open Microsoft.WindowsAzure.Storage.Table
-open System.Web
+open LinqToTwitter
+open Newtonsoft.Json
 
 type Settings =
     { TwitterApiKey : string
@@ -31,13 +20,13 @@ type Settings =
 
     static member load () = 
         { TwitterApiKey =
-            Environment.GetEnvironmentVariable("APPSETTING_twitterapikey", EnvironmentVariableTarget.Process)
+            Environment.GetEnvironmentVariable("twitterapikey", EnvironmentVariableTarget.Process)
           TwitterApiSecret =
-            Environment.GetEnvironmentVariable("APPSETTING_twitterapisecret", EnvironmentVariableTarget.Process)
+            Environment.GetEnvironmentVariable("twitterapisecret", EnvironmentVariableTarget.Process)
           TwitterAccessToken =
-            Environment.GetEnvironmentVariable("APPSETTING_twitteraccesstoken", EnvironmentVariableTarget.Process)
+            Environment.GetEnvironmentVariable("twitteraccesstoken", EnvironmentVariableTarget.Process)
           TwitterAccessTokenSecret =
-            Environment.GetEnvironmentVariable("APPSETTING_twitteraccesstokensecret", EnvironmentVariableTarget.Process)
+            Environment.GetEnvironmentVariable("twitteraccesstokensecret", EnvironmentVariableTarget.Process)
         }
 
 [<CLIMutable>]
